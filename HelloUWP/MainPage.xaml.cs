4aed009d7f1c3b359c528fb14c0382bb393b01ed
Kind Roach <kindroach@hotmail.com>
2016-07-31 13:09:53 +0800

    Add HelloUWP project.

--- HelloUWP/MainPage.xaml.cs
+++ HelloUWP/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace HelloUWP
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sayHelloTextBox.Text = "Hello,UWP!";
+        }
     }
 }
